0b23cb44fc93fc6764379c98b348090da7a05918
Michael Kourlas <michael@kourlas.com>
2018-10-11 08:25:04 -0400

    Fix Travis build

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,25 @@ gulp.task("dev", gulp.series("clean", function() {
 }));
 
 var testTsProject = typescript.createProject("test/tsconfig.json");
-var test = function() {
+
+function testProd() {
+    return testTsProject.src()
+                        .pipe(tslint())
+                        .pipe(tslint.report())
+                        .pipe(testTsProject())
+                        .on("error", function() {
+                            this.on("finish", function() {
+                                process.exit(1);
+                            });
+                        })
+                        .pipe(gulp.dest("test/lib"))
+                        .pipe(mocha());
+}
+
+gulp.task("test-prod", gulp.series(gulp.parallel("prod", "clean-test"),
+                                   testProd));
+
+function testDev() {
     return testTsProject.src()
                         .pipe(tslint())
                         .pipe(tslint.report())
@@ -85,11 +103,11 @@ var test = function() {
                         })
                         .pipe(sourcemaps.write())
                         .pipe(gulp.dest("test/lib"))
-                        .pipe(mocha())
-};
-gulp.task("test", gulp.series("prod", "clean-test", test));
-gulp.task("test-prod", gulp.series("prod", "clean-test", test));
-gulp.task("test-dev", gulp.series("dev", "clean-test", test));
+                        .pipe(mocha());
+}
+
+gulp.task("test-dev", gulp.series(gulp.parallel("dev", "clean-test"), testDev));
+gulp.task("test", gulp.series("test-prod"));
 
 var docOptions = {
     mode: "file",
@@ -98,9 +116,15 @@ var docOptions = {
     target: "es5",
     tsconfig: "tsconfig.json"
 };
-gulp.task("docs", gulp.series(gulp.parallel("prod", "clean-docs"), function() {
+
+function docs() {
     return gulp.src("src")
                .pipe(typedoc(docOptions));
-}));
+}
 
-gulp.task("default", gulp.series("test-prod", "docs"));
+gulp.task("docs", gulp.series(gulp.parallel("prod", "clean-docs"), docs));
+
+gulp.task("default",
+          gulp.series("prod",
+                      gulp.parallel(gulp.series("clean-test", testProd),
+                                    gulp.series("clean-docs", docs))));
