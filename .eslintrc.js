3aa36495c5e59cb9938223b86d896629e1304942
Michael Kourlas <michael@kourlas.com>
2020-02-02 09:40:48 -0500

    Remove trailing comma

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,6 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 0,
 
         // Allow private functions at bottom of file
-        '@typescript-eslint/no-use-before-define': 0,
+        '@typescript-eslint/no-use-before-define': 0
     }
 };
