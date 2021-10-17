0be97583c13830f9c0d89100d1ecbabd9f4fcfe1
Michael Kourlas <michael@kourlas.com>
2021-10-17 13:19:36 -0400

    Fix linting issue

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,14 @@
 
 import XmlDocument, {IXmlDocumentOptions} from "./nodes/XmlDocument";
 
-export {default as XmlAttribute, IXmlAttributeOptions} from "./nodes/XmlAttribute";
-export {default as XmlAttributeText, IXmlAttributeTextOptions} from "./nodes/XmlAttributeText";
+export {
+    default as XmlAttribute,
+    IXmlAttributeOptions
+} from "./nodes/XmlAttribute";
+export {
+    default as XmlAttributeText,
+    IXmlAttributeTextOptions
+} from "./nodes/XmlAttributeText";
 export {default as XmlCdata, IXmlCdataOptions} from "./nodes/XmlCdata";
 export {default as XmlCharData, IXmlCharDataOptions} from "./nodes/XmlCharData";
 export {default as XmlCharRef, IXmlCharRefOptions} from "./nodes/XmlCharRef";
@@ -25,13 +31,31 @@ export {default as XmlComment, IXmlCommentOptions} from "./nodes/XmlComment";
 export {default as XmlDecl, IXmlDeclOptions} from "./nodes/XmlDecl";
 export {default as XmlDocument, IXmlDocumentOptions} from "./nodes/XmlDocument";
 export {default as XmlDtd, IXmlDtdOptions} from "./nodes/XmlDtd";
-export {default as XmlDtdAttlist, IXmlDtdAttlistOptions} from "./nodes/XmlDtdAttlist";
-export {default as XmlDtdElement, IXmlDtdElementOptions} from "./nodes/XmlDtdElement";
-export {default as XmlDtdEntity, IXmlDtdEntityOptions} from "./nodes/XmlDtdEntity";
-export {default as XmlDtdNotation, IXmlDtdNotationOptions} from "./nodes/XmlDtdNotation";
-export {default as XmlDtdParamEntityRef, IXmlDtdParamEntityRefOptions} from "./nodes/XmlDtdParamEntityRef";
+export {
+    default as XmlDtdAttlist,
+    IXmlDtdAttlistOptions
+} from "./nodes/XmlDtdAttlist";
+export {
+    default as XmlDtdElement,
+    IXmlDtdElementOptions
+} from "./nodes/XmlDtdElement";
+export {
+    default as XmlDtdEntity,
+    IXmlDtdEntityOptions
+} from "./nodes/XmlDtdEntity";
+export {
+    default as XmlDtdNotation,
+    IXmlDtdNotationOptions
+} from "./nodes/XmlDtdNotation";
+export {
+    default as XmlDtdParamEntityRef,
+    IXmlDtdParamEntityRefOptions
+} from "./nodes/XmlDtdParamEntityRef";
 export {default as XmlElement, IXmlElementOptions} from "./nodes/XmlElement";
-export {default as XmlEntityRef, IXmlEntityRefOptions} from "./nodes/XmlEntityRef";
+export {
+    default as XmlEntityRef,
+    IXmlEntityRefOptions
+} from "./nodes/XmlEntityRef";
 export {default as XmlProcInst, IXmlProcInstOptions} from "./nodes/XmlProcInst";
 export {IStringOptions} from "./options"
 
